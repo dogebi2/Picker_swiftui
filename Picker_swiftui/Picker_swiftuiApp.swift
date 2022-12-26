4fa2132bc7c9a353e4d2bb7470a4479f9403c99f
dogebi2 <rainxlh@gmail.com>
2022-12-26 16:46:25 +0800

    Initial Commit

---
+++ Picker_swiftui/Picker_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Picker_swiftuiApp.swift
+//  Picker_swiftui
+//
+//  Created by rain ing on 2022/12/26.
+//
+
+import SwiftUI
+
+@main
+struct Picker_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
